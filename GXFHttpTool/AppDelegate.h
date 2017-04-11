5699fa79990da8415f530c949f4cf4d8c9555268
guxuefei <xuefeigu@163.com>
2017-04-11 10:02:35 +0800

    Initial Commit

---
+++ GXFHttpTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GXFHttpTool
+//
+//  Created by 顾雪飞 on 17/4/11.
+//  Copyright © 2017年 顾雪飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
